6791545c0f061d46bdb55be690102a16738801b0
Hariom Chouhan <hariomchouhan430@gmail.com>
2024-06-30 12:38:33 +0530

    environment variable best practice to retrive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-
+import conf from "./conf/conf";
 function App() {
-  console.log(process.env.REACT_APP_APPWRITE_URL);
+  console.log(conf.appwriteBucketId);
   return (
     <>
       <h1 className="text-4xl text-center font-bold">A blog app with appwrite</h1>
