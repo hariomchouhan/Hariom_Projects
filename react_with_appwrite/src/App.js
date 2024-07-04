a32ced0104e8785569b11d8dcda2247530ee7eff
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-07-04 14:01:32 +0000

    Production grade react components like Input and Button

---
+++ react_with_appwrite/src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .catch((error) => {
+        console.log("error ", error);
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
